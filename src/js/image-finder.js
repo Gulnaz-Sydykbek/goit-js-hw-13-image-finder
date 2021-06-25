5d946228e500d5d2dac4dff4147a1612e56c2b50
Gulnaz-Sydykbek <gulnaz.sydykbekova.94@mail.ru>
2021-06-25 17:31:15 +0600

    hw-13-5

--- src/js/image-finder.js
+++ src/js/image-finder.js
@@ -1,4 +1,4 @@
-/*import ImagesApiService from './image-finder-service';
+import ImagesApiService from './image-finder-service';
 import refs from './refs-image-finder';
 import imagesTpl from '../templates/image-finder.hbs';
 
@@ -52,72 +52,7 @@ function appendImagesMarkup(images) {
 
 function clearImagesContainer() {
   refs.imagesContainer.innerHTML = '';
-}
-
-
-const callback = entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting && imagesApiService.query !== '') {
-      imagesApiService.fetchImages().then(appendImagesMarkup);
-    }
-  });
-};
-
-const options = {
-  rootMargin: '150px',
-};
-
-const observer = new IntersectionObserver(callback, options);*/
-
-//============================================================
-
-import ImagesApiService from './image-finder-service';
-import refs from './refs-image-finder';
-import imagesTpl from '../templates/image-finder.hbs';
-
-import '@pnotify/core/dist/BrightTheme.css';
-import { success } from '@pnotify/core';
-import { error } from '@pnotify/core';
-
-const imagesApiService = new ImagesApiService();
-
-refs.searchForm.addEventListener('submit', onSearch);
-
-
-function onSearch(e) {
-  e.preventDefault();
-
-  const form = e.currentTarget;
-  imagesApiService.query = form.elements.query.value;
-
-  form.reset();
-  clearImagesContainer();
-  imagesApiService.resetPage();
-  observer.observe(refs.sentinel);
-}
-
-function appendImagesMarkup(images) {
-  if (images.length > 1) {
-    success({
-      text: 'Successful loading of images!',
-      addClass: 'success',
-      delay: 500,
-    });
-  }
-
-  if (images.length === 0) {
-    error({
-      text: 'Not found!',
-      addClass: 'success',
-      delay: 500,
-    });
-  }
-  const markup = imagesTpl(images);
-  refs.imagesContainer.insertAdjacentHTML('beforeend', markup);
-}
-
-function clearImagesContainer() {
-  refs.imagesContainer.innerHTML = '';
+  observer.unobserve(refs.sentinel)
 }
 
 
